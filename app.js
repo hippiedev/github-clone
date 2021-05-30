f6768363bf286d95589f9eb77ceb409c44c87fb0
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2021-05-30 16:58:31 +0200

    encode token

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const username = localStorage.getItem("username")
+const username = localStorage.getItem("username");
 
 let content = {
   query: `query($username: String!) {
@@ -6,7 +6,12 @@ let content = {
       name
     	avatarUrl
     	bio
-      repositories(first: 20, isFork: false, orderBy: {field: CREATED_AT, direction: DESC}) {
+      status {
+        emojiHTML
+        message
+        __typename
+      }
+      repositories(first: 20, isFork: false, privacy:PUBLIC, ownerAffiliations:OWNER, orderBy: {field: UPDATED_AT, direction: DESC}) {
         nodes {
           id
           name
@@ -17,6 +22,7 @@ let content = {
           forkCount
           primaryLanguage {
             name
+            color
           }
         }
       }
@@ -25,11 +31,35 @@ let content = {
   `,
   variables: `{
     "username": "${username}"
-  }`
+  }`,
 };
 
 let body = JSON.stringify(content);
-let token = API_KEY;
+
+let token = config.MY_API_TOKEN;
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const formatDate = (val) => {
+  const dt = new Date(val);
+  const month = months[dt.getMonth()];
+  const date = dt.getDate();
+  const year = dt.getFullYear();
+  const fDate = date < 10 ? `0${date}` : date;
+  return `${fDate} ${month}`;
+};
 
 fetch("https://api.github.com/graphql", {
   method: "post",
@@ -45,29 +75,38 @@ fetch("https://api.github.com/graphql", {
     const html = data.data.user.repositories.nodes
       .map((repo) => {
         return `
-        <div id="repo_container" key="${repo.id}"> 
-          <h3>${repo.name}</h3>
+          <div id="repo_container" key="${repo.id}">
+          <button>
+            <svg class="octicon octicon-star mr-1" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+              <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z">
+              </path>
+            </svg>
+            <span>Star</span>
+          </button>
+          <h3><a href="${repo.url}">${repo.name}</a></h3>
+          
           ${repo.description == null ? "" : `<p>${repo.description}</p>`}
             
           <div id="subrepo_text">
             ${
               repo.primaryLanguage == null
                 ? ""
-                : 
-                `<span>
-                ${
-                    repo.primaryLanguage == null
-                      ? ""
-                      : repo.primaryLanguage.name
-                  }
+                : `<span>
+                <div class="repo-language-color" style="background-color: ${
+                  repo.primaryLanguage.color
+                }"></div>
+                ${repo.primaryLanguage == null ? "" : repo.primaryLanguage.name}
                   </span>`
             }
             ${
               repo.stargazerCount == 0
                 ? ""
-                : `<span><svg aria-label="star" role="img" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-star">
+                : `
+              <span>
+              <svg aria-label="star" role="img" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" class="octicon octicon-star">
             <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
-        </svg>${repo.stargazerCount}</span>`
+        </svg>${repo.stargazerCount}
+              </span>`
             }
             
         ${
@@ -78,27 +117,37 @@ fetch("https://api.github.com/graphql", {
     </svg>${repo.forkCount}</span>`
         }
         
-            <span>${repo.updatedAt}</span>
+            <span class="date">Updated on ${formatDate(repo.updatedAt)}</span>
 
           </div>
         </div>`;
       })
       .join("");
 
-    let sub_userthumb = 
-    `<img class="user_thumb" src="${data.data.user.avatarUrl}"/>
+    let sub_userthumb = `<img class="user_thumb" src="${data.data.user.avatarUrl}"/>
       <span class="dropdown-caret"></span>`;
 
-    document.querySelector("#repo").insertAdjacentHTML("afterbegin", html);
-    document.querySelector("#user").insertAdjacentHTML("afterbegin", sub_userthumb);
-    document.querySelector("#name").insertAdjacentHTML("afterbegin", data.data.user.name);
-    document.querySelector("#bio").insertAdjacentHTML("beforeend", data.data.user.bio);
-    document.querySelector("#username").insertAdjacentHTML("beforeend", username);
-    document.querySelector("#title").insertAdjacentHTML("afterbegin", data.data.user.name);
-    
+    document.querySelector("#repo").insertAdjacentHTML("beforeend", html);
+    document
+      .querySelector("#user")
+      .insertAdjacentHTML("afterbegin", sub_userthumb);
+    document
+      .querySelector("#name")
+      .insertAdjacentHTML("afterbegin", data.data.user.name);
+    document
+      .querySelector("#bio")
+      .insertAdjacentHTML("beforeend", data.data.user.bio);
+    document
+      .querySelector("#username")
+      .insertAdjacentHTML("beforeend", username);
+    document
+      .querySelector("#title")
+      .insertAdjacentHTML(
+        "afterbegin",
+        `${username} (${data.data.user.name}) / Repositories`
+      );
+
     const thumb = document.querySelector(".thumbnail");
+
     thumb.style.backgroundImage = `url('${data.data.user.avatarUrl}')`;
-
-
-    
   });
---
+++ config.js
@@ -0,0 +1,5 @@
+let encodedToken = "Z2hwX0t5d2hPa2xwY2J1dG5xY1ZWVUJHSXBGRlpqWGlOSzBEN05oSg==";
+const decodedToken = window.atob(encodedToken);
+var config = {
+  MY_API_TOKEN: decodedToken,
+};
